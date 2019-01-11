c9586fbae08bfbee697fa410337be37a55ca5934
xiehuang <huangcrab@xiehuangs-MacBook-Pro.local>
2019-01-11 01:43:36 -0500

    push

---
+++ precache-manifest.29f6bb599d5dca6e71c0f667a593414d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0344dc17f983a892000",
+    "url": "/static/js/main.c0344dc1.chunk.js"
+  },
+  {
+    "revision": "ef491cf7f7805206ad3a",
+    "url": "/static/js/1.ef491cf7.chunk.js"
+  },
+  {
+    "revision": "c0344dc17f983a892000",
+    "url": "/static/css/main.32419979.chunk.css"
+  },
+  {
+    "revision": "988ce88354a86a6e75b9eb5753f57661",
+    "url": "/index.html"
+  }
+];
